8ab6ed91fa124f7ecc6001e73eaa9cfa26122c32
Jason Kang <jasonkang14@gmail.com>
2024-03-21 17:10:31 +0900

    Add setup.py

--- streamlit_cookies_manager/setup.py
+++ streamlit_cookies_manager/setup.py
@@ -1,16 +1,36 @@
-from setuptools import setup
-from setuptools.command.install import install
-import subprocess
+from pathlib import Path
 
+import setuptools
 
-class CustomInstallCommand(install):
-    """Customized setuptools install command to build the React app."""
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
-    def run(self):
-        install.run(self)  # Run the standard install
-        subprocess.check_call("cd react_app_directory && npm ci && npm run build", shell=True)
-
-
-setup(cmdclass={
-    'install': CustomInstallCommand,
-}, )
+setuptools.setup(
+    name="52g-streamlit-cookie-component",
+    version="0.0.1",
+    author="Jason Kang",
+    author_email="jasonkang14@gmail.com",
+    description="Streamlit component which allows you to save auth token",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.7",
+    install_requires=[
+        # By definition, a Custom Component depends on Streamlit.
+        # If your component has other Python dependencies, list
+        # them here.
+        "streamlit >= 0.63",
+    ],
+    extras_require={
+        "devel": [
+            "wheel",
+            "pytest==7.4.0",
+            "playwright==1.39.0",
+            "requests==2.31.0",
+            "pytest-playwright-snapshot==1.0",
+            "pytest-rerunfailures==12.0",
+        ]
+    })
